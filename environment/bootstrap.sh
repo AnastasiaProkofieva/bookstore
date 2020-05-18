d1123f0fb740e4be3ef3eefb5c4c0aaf2bcca124
Anastasia <Anastasia.beetroot@gmail.com>
2020-05-18 13:37:15 +0300

    changes in env

--- environment/bootstrap.sh
+++ environment/bootstrap.sh
@@ -5,4 +5,6 @@ apt install -y php
 apt install -y mysql-server
 apt install -y php-xdebug php-xml php-mbstring php-curl
 cp /var/www/html/environment/xdebug.ini /etc/php/7.2/cli/conf.d/20-xdebug.ini
+cp /var/www/html/environment/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 systemctl reload apache2
+systemctl restart mysql
